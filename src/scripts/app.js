33bf59142a67cbca1cf816bc7d30c05c89be97af
Matchu <matchu1993@gmail.com>
2021-12-04 23:03:50 -0800

    Refactor app.js into App class
    There's enough shared state happening that I think this will be useful now!
    
    I also added error messages if the container or roomId are missing!

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,26 +1,39 @@
 import ChatView from "./chat-view";
 import MatrixClient from "./matrix-client";
 
-async function startApp() {
-  const container = document.querySelector("chat-panel");
-  const roomId = document.querySelector(
-    "meta[name='playroom:room-id']"
-  ).content;
+class App {
+  constructor({ container, roomId }) {
+    this.container = container;
+    this.roomId = roomId;
+    this.chatView = new ChatView({ container, roomId });
+    // TODO: Infer homeserver from roomId!
+    this.client = new MatrixClient({ homeserver: "matrix.org", roomId });
+  }
 
-  const chatView = new ChatView({ container, roomId });
-  chatView.mount();
-  console.debug("ChatView", chatView);
+  async start() {
+    this.chatView.mount();
+    await this.login();
+  }
 
-  try {
-    const client = new MatrixClient({ homeserver: "matrix.org", roomId });
-    const session = await client.loginAsSavedSessionOrGuest();
-    await chatView.useSession(session);
-    console.debug("MatrixClient", client);
-  } catch (error) {
+  async login() {
+    try {
+      const session = await this.client.loginAsSavedSessionOrGuest();
+      await this.chatView.useSession(session);
+    } catch (error) {
+      this.handleLoginError(error);
+      return;
+    }
+
+    this.container.setAttribute("status", "ready");
+    this.container.removeAttribute("loading-step");
+  }
+
+  handleLoginError(error) {
     console.error(error);
+
     if (error.consentUrl) {
       try {
-        handleConsentError(error, container);
+        this.handleConsentError(error);
         return;
       } catch (error2) {
         console.warn(
@@ -30,25 +43,47 @@ async function startApp() {
       }
     }
 
-    container.setAttribute("status", "error");
-    container.setAttribute("error-type", "login-error");
+    this.container.setAttribute("status", "error");
+    this.container.setAttribute("error-type", "login-error");
+  }
+
+  handleConsentError(error) {
+    const links = this.container.querySelectorAll(
+      "a[href$='terms-placeholder.html']"
+    );
+    if (links.length === 0) {
+      throw new Error(`could not find terms-placeholder.html link to replace`);
+    }
+    for (const link of links) {
+      link.href = error.consentUrl;
+    }
+    this.container.setAttribute("status", "error");
+    this.container.setAttribute("error-type", "must-agree-to-terms");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.querySelector("chat-panel");
+  if (container == null) {
+    alert("Error: Can't find the chat-panel element to mount the app into.");
     return;
   }
 
-  container.setAttribute("status", "ready");
-  container.removeAttribute("loading-step");
-}
-
-function handleConsentError(error, container) {
-  const links = container.querySelectorAll("a[href$='terms-placeholder.html']");
-  if (links.length === 0) {
-    throw new Error(`could not find terms-placeholder.html link to replace`);
+  const roomId = document.querySelector(
+    "meta[name='playroom:room-id']"
+  )?.content;
+  if (roomId == null) {
+    alert(
+      `Error: Can't find the room ID. It should be configured in the <head> ` +
+        `of the document as <meta name="playroom:room-id" ` +
+        `content="ROOM_ID_HERE" />.`
+    );
+    return;
   }
-  for (const link of links) {
-    link.href = error.consentUrl;
-  }
-  container.setAttribute("status", "error");
-  container.setAttribute("error-type", "must-agree-to-terms");
-}
 
-document.addEventListener("DOMContentLoaded", () => startApp());
+  const app = new App({ container, roomId });
+
+  console.debug("App", app);
+
+  await app.start();
+});
