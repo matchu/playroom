c9b7fb8d0d07b4604e9e7a7a96e36b858b27e57f
Matchu <matchu1993@gmail.com>
2021-12-04 22:52:28 -0800

    Link to terms, no auto-reload yet

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -17,10 +17,18 @@ async function startApp() {
     await chatView.useSession(session);
     console.debug("MatrixClient", client);
   } catch (error) {
-    if (error.consentUrl) {
-      window.open(error.consentUrl);
-    }
     console.error(error);
+    if (error.consentUrl) {
+      try {
+        handleConsentError(error, container);
+        return;
+      } catch (error2) {
+        console.warn(
+          `Could not show consent prompt, showing generic error instead`,
+          error2
+        );
+      }
+    }
 
     container.setAttribute("status", "error");
     container.setAttribute("error-type", "login-error");
@@ -31,4 +39,16 @@ async function startApp() {
   container.removeAttribute("loading-step");
 }
 
-startApp().catch((error) => console.error(error));
+function handleConsentError(error, container) {
+  const links = container.querySelectorAll("a[href$='terms-placeholder.html']");
+  if (links.length === 0) {
+    throw new Error(`could not find terms-placeholder.html link to replace`);
+  }
+  for (const link of links) {
+    link.href = error.consentUrl;
+  }
+  container.setAttribute("status", "error");
+  container.setAttribute("error-type", "must-agree-to-terms");
+}
+
+document.addEventListener("DOMContentLoaded", () => startApp());
