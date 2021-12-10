d95eadd94b29770e546bf90818ee33de249e265b
Matchu <matchu1993@gmail.com>
2021-12-10 01:10:58 -0800

    Better app startup JS
    We have an error handler, and use the `defer` attribute to avoid having to set the DOM-ready event listener.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,17 @@ import Playroom from "./model/playroom";
 import { createApp } from "./lib/petite-vue";
 import Hydrogen from "./components/hydrogen";
 
-function mountPlayroomApp({ roomId }) {
+try {
+  const roomIdMetaTag = document.querySelector("meta[name='playroom:room-id']");
+  const roomId = roomIdMetaTag?.content;
+  if (roomId == null) {
+    throw new Error(
+      `Can't find the room ID. It should be configured in the <head> ` +
+        `of the document as <meta name="playroom:room-id" ` +
+        `content="ROOM_ID_HERE" />.`
+    );
+  }
+
   const playroom = new Playroom({ roomId });
 
   createApp({
@@ -14,19 +24,10 @@ function mountPlayroomApp({ roomId }) {
   }).mount();
 
   playroom.start();
+} catch (error) {
+  console.error(error);
+  alert(
+    `Error starting the stream, sorry! If you keep getting this, please let ` +
+      `me know! You can open the browser console for details.`
+  );
 }
-
-document.addEventListener("DOMContentLoaded", async () => {
-  const roomIdMetaTag = document.querySelector("meta[name='playroom:room-id']");
-  const roomId = roomIdMetaTag?.content;
-  if (roomId == null) {
-    alert(
-      `Error: Can't find the room ID. It should be configured in the <head> ` +
-        `of the document as <meta name="playroom:room-id" ` +
-        `content="ROOM_ID_HERE" />.`
-    );
-    return;
-  }
-
-  mountPlayroomApp({ roomId });
-});
