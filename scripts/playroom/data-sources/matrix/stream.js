d56dd525f75f40c7225e809472e84a138f48dace
Matchu <matchu1993@gmail.com>
2021-12-20 01:36:11 -0800

    Fix security check for broadcast events
    Okay, right, we're not using widgets anymore, so the event type needs to change.
    
    But I also learned about the `users_default` and `state_default` settings, which control the settings when the broadcast event isn't specified! This will make it easier for people to edit these settings in their own clients, and means that it comes with pretty sensible defaults, which is nice!

--- scripts/playroom/data-sources/matrix/stream.js
+++ scripts/playroom/data-sources/matrix/stream.js
@@ -10,18 +10,34 @@ export async function loadStreamState({ settings, session }) {
   const powerLevelsEvent = roomEvents.filter(
     (e) => e.type === "m.room.power_levels"
   )[0];
+  // These are the default power levels for new users, and for unspecified
+  // state event types, respectively. The fallback numbers are copied from the
+  // Matrix spec, as the definitive fallback values if not specified.
+  const defaultUserPowerLevel = powerLevelsEvent?.content?.users_default ?? 0;
+  const defaultPowerLevelToManageState =
+    powerLevelsEvent?.content?.state_default ?? 50;
   const powerLevelToManageWidgets =
-    powerLevelsEvent?.content?.events?.["im.vector.modular.widgets"] || 0;
+    powerLevelsEvent?.content?.events?.["dev.playroom.broadcast"] ??
+    defaultPowerLevelToManageState;
   const currentUserPowerLevel =
-    powerLevelsEvent?.content?.users?.[session.userId] || 0;
+    powerLevelsEvent?.content?.users?.[session.userId] ?? defaultUserPowerLevel;
   const canManage = currentUserPowerLevel >= powerLevelToManageWidgets;
 
+  // SECURITY TODO: Rooms on matrix.org start with Moderator access for "Change
+  //       "Settings" by default. Should we warn to advise people to increase
+  //       it to Admin? Perhaps warn more softly? Moderators are trusted, but
+  //       are they *that* trusted? We could also consider automatically adding
+  //       a setting for dev.playroom.broadcast specifically if not present…
+  // NOTE: I suppose the text doesn't quite match the condition: if you have a
+  //       custom setting for broadcast events = 0, we'll show this warning,
+  //       which isn't quite true. But that seems unlikely, and this message
+  //       is fine.
   const securityWarning =
-    powerLevelToManageWidgets === 0
+    powerLevelToManageWidgets === defaultUserPowerLevel
       ? `Your Matrix room is currently configured to let *anybody* ` +
-        `add and edit widgets.\n\nPlease fix this by setting the "Modify ` +
-        `widgets" setting (or "im.vector.modular.widgets" setting) to ` +
-        `require moderator access.`
+        `send room state events, including starting a broadcast!\n\nPlease ` +
+        `fix this by setting the "Change settings" setting (or a custom ` +
+        `"dev.playroom.broadcast" setting) to require admin access.`
       : null;
 
   const broadcastEvent = roomEvents.find(
